67111c73280c7580a05f9dcc0f773273544f8ecb
eels <ely.eels@gmail.com>
2019-10-25 12:59:46 -0400

    housekeeping

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
     // Find and copy any `jpg` files, maintaining directory structure.
-  eleventyConfig.addPassthroughCopy("*.jpg");
-  return {
-    templateFormats: [
-      "md",
-      "css" // css is not yet a recognized template extension in Eleventy
-    ]
-  };
+    eleventyConfig.addPassthroughCopy("*.jpg");
+    return {
+        templateFormats: [
+            "md",
+            "css" // css is not yet a recognized template extension in Eleventy
+        ]
+    };
 };
