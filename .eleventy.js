e2d6895c289f9bd0e08576b9d92ebc990d8f991e
eels <ely.eels@gmail.com>
2019-11-07 17:35:53 -0500

    fixed reponsive, only firefox

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,18 @@
-// const { DateTime } = require("luxon");
-// const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
-// Import fast-glob package
-// const fg = require("fast-glob");
-
-// const bookImages = fg.sync(["**/library/*", "!**/_site"]);
-
 module.exports = function(eleventyConfig) {
     // lazy loading on hold - need work around for responsive images/unknown height
     // eleventyConfig.addPlugin(lazyImagesPlugin);
 
-    // does not seem to be working
-    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    // eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    //     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
-    // });
-    
+    module.exports = function({ collections }) {
+        return `<ul>
+        ${collections.page.map((page) => `<li>${ page.data.tags }</li>`).join("\n")}
+        </ul>`;
+    };
+
     // Find and copy any `jpg` files, maintaining directory structure.
     eleventyConfig.addPassthroughCopy("library/_img/*.jpg");
     // eleventyConfig.addCollection("library", function(collection) {
     // return bookImages;
-  // });
+    // });
     return {
         templateFormats: [
             "jpg",
@@ -28,5 +21,4 @@ module.exports = function(eleventyConfig) {
         ]
     };
 };
-
 
